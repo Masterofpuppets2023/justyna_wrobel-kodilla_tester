15317c5d872135e76f3c139052cab00e427033a3
Justyna Wróbel <justyna.wrobel.1985@gmail.com>
2023-01-15 21:01:02 +0100

    zmienne

---
+++ kodilla-intro/src/main/java/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        String myName = "Justyna";
+        int myAge = 37;
+        double height = 1.74;
+        char carClass = 'W';
+        boolean isGoodProgrammer = true;
+        System.out.println(myName);
+    }
+}
---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,16 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String mySurname = "Sparrow";
+        int myKids = 3;
+        double feet = 39.5;
+        char firstName = 'J';
+        boolean isWoman = true;
+        boolean haveDog = false;
+        System.out.println(mySurname);
+        System.out.println(myKids);
+        System.out.println(feet);
+        System.out.println(firstName);
+        System.out.println(isWoman);
+        System.out.println(haveDog);
+    }
+}

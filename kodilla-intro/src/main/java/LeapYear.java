95008cbfc9f5a1e2bf21f497e08fa8a309d02f0b
Justyna Wróbel <justyna.wrobel.1985@gmail.com>
2023-01-18 21:29:50 +0100

    isLeapYear

--- kodilla-intro/src/main/java/LeapYear.java
+++ kodilla-intro/src/main/java/LeapYear.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class LeapYear {
+public class LeapYear {
+    public static void main(String[] args) {
+        if (isLeapYear(1988)) {
+            System.out.println("isLeapYear");
+        } else {
+            System.out.println("isNotLeapYear");
+        }
+    }
+
+    public static boolean isLeapYear(int year) {
+
+        boolean p4 = (year % 4 == 0);
+        boolean p100 = (year % 100 == 0);
+        boolean p400 = (year % 400 == 0);
+
+        return (p400 || (p4 && !p100));
+    }
 }
+
+

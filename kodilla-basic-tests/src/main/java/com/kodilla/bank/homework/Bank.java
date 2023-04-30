05a12826d71c68f4afd398574dbfe506f656c047
Justyna Wróbel <justyna.wrobel.1985@gmail.com>
2023-04-30 19:15:15 +0200

    testy jednostkowe- zadanie bankomat

--- kodilla-basic-tests/src/main/java/com/kodilla/bank/homework/Bank.java
+++ kodilla-basic-tests/src/main/java/com/kodilla/bank/homework/Bank.java
@@ -1,2 +1,94 @@
-package com.kodilla.bank.homework;public class Bank {
+package com.kodilla.bank.homework;
+
+public class Bank {
+    private CashMachine[] cashMachines;
+
+    public CashMachine[] getCashMachines() {
+        return cashMachines;
+    }
+
+    public Bank(CashMachine[] cashMachines) {
+        this.cashMachines = cashMachines;
+                /*new CashMachine[3];
+        CashMachine machine = new CashMachine();
+        machine.deposit(1000);
+        machine.withdraw(100);
+        machine.withdraw(80);
+        machine.withdraw(120);
+        this.cashMachines[0] = machine;
+
+        machine = new CashMachine();
+        machine.deposit(5000);
+        machine.withdraw(1000);
+        machine.withdraw(600);
+        this.cashMachines[1] = machine;
+
+        machine = new CashMachine();
+        machine.deposit(8000);
+        machine.withdraw(3000);
+        machine.withdraw(100);
+        machine.withdraw(550);
+        this.cashMachines[2] = machine;*/
+    }
+
+    public int totalBalance() {
+        int balance = 0;
+        for (int i=0; i < cashMachines.length; i++) {
+            balance += cashMachines[i].balance();
+        }
+        return balance;
+    }
+
+    public int withdrawalCount() {
+        int count = 0;
+        for (int i=0; i < cashMachines.length; i++) {
+            count += cashMachines[i].getWithdrawalCount();
+        }
+        return count;
+    }
+
+    public int depositCount() {
+        int count = 0;
+        for (int i=0; i < cashMachines.length; i++) {
+            count += cashMachines[i].getDepositCount();
+        }
+        return count;
+    }
+    public double averageWithdrawalAmount() {
+        if (cashMachines.length == 0) {
+            return 0;
+        }
+        int amount = 0;
+        int count = 0;
+        for (int i=0; i < cashMachines.length; i++) {
+            int[] withdrawals = cashMachines[i].getWithdrawals();
+            count += withdrawals.length;
+            for (int j=0; j < withdrawals.length; j++) {
+                amount += (-withdrawals[j]);
+            }
+        }
+        if (count == 0) {
+            return 0;
+        }
+        return amount/count;
+    }
+
+    public double averageDepositAmount() {
+        if (cashMachines.length == 0) {
+            return 0;
+        }
+        int amount = 0;
+        int count = 0;
+        for (int i=0; i < cashMachines.length; i++) {
+            int[] deposits = cashMachines[i].getDesposits();
+            count += deposits.length;
+            for (int j=0; j < deposits.length; j++) {
+                amount += deposits[j];
+            }
+        }
+        if (count == 0) {
+            return 0;
+        }
+        return amount/count;
+    }
 }
--- kodilla-basic-tests/src/test/java/com/kodilla/bank/homework/CashMashineTestSuite.java
+++ kodilla-basic-tests/src/test/java/com/kodilla/bank/homework/CashMashineTestSuite.java
@@ -1,2 +1,78 @@
-package com.kodilla.bank.homework;public class CashMashineTestSuite {
+package com.kodilla.bank.homework;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CashMashineTestSuite {
+    @Test
+    public void shouldHaveZeroTransactions() {
+        CashMachine machine = new CashMachine();
+        int count = machine.getTransactionCount();
+        assertEquals (0, count);
+    }
+    @Test
+    public void shouldHaveThreeTransactions() {
+        CashMachine machine = new CashMachine();
+        machine.deposit(1000);
+        machine.withdraw(100);
+        machine.withdraw(80);
+
+        int count = machine.getTransactionCount();
+        assertEquals (3, count);
+    }
+    @Test
+    public void shouldHaveTwoWithdrawals() {
+        CashMachine machine = new CashMachine();
+        machine.deposit(1000);
+        machine.withdraw(100);
+        machine.withdraw(80);
+
+        int count = machine.getWithdrawalCount();
+        assertEquals (2, count);
+    }
+    @Test
+    public void shouldHaveOneDeposit() {
+        CashMachine machine = new CashMachine();
+        machine.deposit(1000);
+        machine.withdraw(100);
+        machine.withdraw(80);
+
+        int count = machine.getDepositCount();
+        assertEquals(1, count);
+    }
+    @Test
+    public void shouldHaveZeroWithdrawals() {
+        CashMachine machine = new CashMachine();
+        machine.deposit(1000);
+        machine.deposit(100);
+        machine.deposit(80);
+
+        int count = machine.getWithdrawalCount();
+        assertEquals(0, count);
+    }
+    @Test
+    public void shouldHaveCorrectAverageWithdrawalAmount() {
+        CashMachine machine = new CashMachine();
+        machine.deposit(6000);
+        machine.withdraw(1000);
+        machine.withdraw(100);
+        machine.deposit(2000);
+        machine.withdraw(400);
+
+        double average = machine.averageWithdrawalAmount();
+        assertEquals(500, average);
+    }
+
+    @Test
+    public void shouldHaveCorrectAverageDepositAmount() {
+        CashMachine machine = new CashMachine();
+        machine.deposit(6000);
+        machine.withdraw(1000);
+        machine.withdraw(100);
+        machine.deposit(2000);
+        machine.withdraw(400);
+
+        double average = machine.averageDepositAmount();
+        assertEquals(4000, average);
+    }
 }
